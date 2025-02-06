84ad8cb303f0d592ee7f5a21f8e720dfbf72f5e6
AcuLuis <21101076@ue.edu.pe>
2025-02-06 13:11:36 -0500

    Interaccion con botones completado

--- src/JUEGO/TICTACTOE.java
+++ src/JUEGO/TICTACTOE.java
@@ -1,10 +1,22 @@
 
 package JUEGO;
 
+import javax.swing.ImageIcon;
+
 public class TICTACTOE extends javax.swing.JFrame {
     
+    String turno="";
+    int[][] matriz = new int[3][3];
+    int cont;
     public TICTACTOE() {
         initComponents();
+        turno="";
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = 2;
+            }
+        }
+        cont=0;
     }
 
     @SuppressWarnings("unchecked")
@@ -31,9 +43,33 @@ public class TICTACTOE extends javax.swing.JFrame {
             }
         });
 
+        btn_dos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_dosActionPerformed(evt);
+            }
+        });
+
+        btn_tres.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_tresActionPerformed(evt);
+            }
+        });
+
         btn_cuatro.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_cuatroActionPerformed(evt);
+            }
+        });
+
+        btn_cinco.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_cincoActionPerformed(evt);
+            }
+        });
+
+        btn_seis.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_seisActionPerformed(evt);
             }
         });
 
@@ -43,8 +79,25 @@ public class TICTACTOE extends javax.swing.JFrame {
             }
         });
 
+        btn_ocho.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_ochoActionPerformed(evt);
+            }
+        });
+
+        btn_nueve.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_nueveActionPerformed(evt);
+            }
+        });
+
         btn_reset.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         btn_reset.setText("RESET");
+        btn_reset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_resetActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -101,16 +154,186 @@ public class TICTACTOE extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btn_cuatroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_cuatroActionPerformed
-        // TODO add your handling code here:
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_cuatro.setIcon(icono);
+            turno="circulo";
+            matriz[1][0]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_cuatro.setIcon(icono);
+            turno="equis";
+            matriz[1][0]=0;
+        }
+        this.btn_cuatro.setEnabled(false);
+        cont++;
     }//GEN-LAST:event_btn_cuatroActionPerformed
 
     private void btn_unoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_unoActionPerformed
-        // TODO add your handling code here:
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_uno.setIcon(icono);
+            turno="circulo";
+            matriz[0][0]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_uno.setIcon(icono);
+            turno="equis";
+            matriz[0][0]=0;
+        }
+        this.btn_uno.setEnabled(false);
+        cont++;
     }//GEN-LAST:event_btn_unoActionPerformed
 
     private void btn_sieteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_sieteActionPerformed
-        // TODO add your handling code here:
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_siete.setIcon(icono);
+            turno="circulo";
+            matriz[2][0]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_siete.setIcon(icono);
+            turno="equis";
+            matriz[2][0]=0;
+        }
+        this.btn_siete.setEnabled(false);
+        cont++;
     }//GEN-LAST:event_btn_sieteActionPerformed
+
+    private void btn_dosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_dosActionPerformed
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_dos.setIcon(icono);
+            turno="circulo";
+            matriz[0][1]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_dos.setIcon(icono);
+            turno="equis";
+            matriz[0][1]=0;
+        }
+        this.btn_dos.setEnabled(false);
+        cont++;
+    }//GEN-LAST:event_btn_dosActionPerformed
+
+    private void btn_tresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_tresActionPerformed
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_tres.setIcon(icono);
+            turno="circulo";
+            matriz[0][2]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_tres.setIcon(icono);
+            turno="equis";
+            matriz[0][2]=0;
+        }
+        this.btn_tres.setEnabled(false);
+        cont++;
+    }//GEN-LAST:event_btn_tresActionPerformed
+
+    private void btn_cincoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_cincoActionPerformed
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_cinco.setIcon(icono);
+            turno="circulo";
+            matriz[1][1]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_cinco.setIcon(icono);
+            turno="equis";
+            matriz[1][1]=0;
+        }
+        this.btn_cinco.setEnabled(false);
+        cont++;
+    }//GEN-LAST:event_btn_cincoActionPerformed
+
+    private void btn_seisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_seisActionPerformed
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_seis.setIcon(icono);
+            turno="circulo";
+            matriz[1][2]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_seis.setIcon(icono);
+            turno="equis";
+            matriz[1][2]=0;
+        }
+        this.btn_seis.setEnabled(false);
+        cont++;
+    }//GEN-LAST:event_btn_seisActionPerformed
+
+    private void btn_ochoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_ochoActionPerformed
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_ocho.setIcon(icono);
+            turno="circulo";
+            matriz[2][1]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_ocho.setIcon(icono);
+            turno="equis";
+            matriz[2][1]=0;
+        }
+        this.btn_ocho.setEnabled(false);
+        cont++;
+    }//GEN-LAST:event_btn_ochoActionPerformed
+
+    private void btn_nueveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_nueveActionPerformed
+        if(turno.equals("") || !turno.equals("circulo")){
+            ImageIcon icono = new ImageIcon("src/IMAGES//equis.png");
+            this.btn_nueve.setIcon(icono);
+            turno="circulo";
+            matriz[2][2]=1;
+        }else{
+            ImageIcon icono = new ImageIcon("src/IMAGES//circulo.png");
+            this.btn_nueve.setIcon(icono);
+            turno="equis";
+            matriz[2][2]=0;
+        }
+        this.btn_nueve.setEnabled(false);
+        cont++;
+    }//GEN-LAST:event_btn_nueveActionPerformed
+    public void resetea(){
+        turno="";
+        cont=0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matriz[i][j] = 2;
+            }
+        }
+        this.btn_uno.setEnabled(true);
+        this.btn_uno.setIcon(null);
+        
+        this.btn_dos.setEnabled(true);
+        this.btn_dos.setIcon(null);
+        
+        this.btn_tres.setEnabled(true);
+        this.btn_tres.setIcon(null);
+        
+        this.btn_cuatro.setEnabled(true);
+        this.btn_cuatro.setIcon(null);
+
+        this.btn_cinco.setEnabled(true);
+        this.btn_cinco.setIcon(null);
+        
+        this.btn_seis.setEnabled(true);
+        this.btn_seis.setIcon(null);
+
+        this.btn_siete.setEnabled(true);
+        this.btn_siete.setIcon(null);
+        
+        this.btn_ocho.setEnabled(true);
+        this.btn_ocho.setIcon(null);
+        
+        this.btn_nueve.setEnabled(true);
+        this.btn_nueve.setIcon(null);
+    }
+    private void btn_resetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_resetActionPerformed
+        this.resetea();
+    }//GEN-LAST:event_btn_resetActionPerformed
 
     /**
      * @param args the command line arguments
